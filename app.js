9198c4627a1594263281ddeb09b640fb71072e82
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-04-06 20:57:23 -0400

    REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = express();
 // app.use(bodyParser.urlencoded()); //x-ww-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Contorl-Allow-Methods",
+    "GET, POST , PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
