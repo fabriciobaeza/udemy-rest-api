4c3247885029708692d531045dcacff18ffb7a78
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-04-11 19:48:04 -0400

    Static Images & Error Handling

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -22,17 +22,16 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: "Validation failed, entered data is incorrect",
-      errors: errors.array(),
-    });
+    const error = new Error("Validation failed, entered data is incorrect");
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/fightclub.jpg',
+    imageUrl: "images/fightclub.jpg",
     creator: { name: "Edge" },
   });
   post
@@ -43,5 +42,10 @@ exports.createPost = (req, res, next) => {
         post: result,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
