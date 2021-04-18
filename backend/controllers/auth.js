bbfdc580ff38cff7bc8bc2ae7f804a534b286fdb
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-04-17 21:29:44 -0400

    Starting with the User Login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -16,16 +16,48 @@ exports.signup = (req, res, next) => {
   const password = req.body.password;
   bcrypt
     .hash(password, 12)
-    .then(hashedPw => {
-        const user = new User({
-            email: email,
-            password: hashedPw,
-            name: name
-        });
-        return user.save()
+    .then((hashedPw) => {
+      const user = new User({
+        email: email,
+        password: hashedPw,
+        name: name,
+      });
+      return user.save();
     })
-    .then(result => {
-        res.statusCode(201).json({message: 'User created!', userId: result._id});
+    .then((result) => {
+      res
+        .statusCode(201)
+        .json({ message: "User created!", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("A user with this email could not be found.");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong Password!");
+        error.statusCode = 401;
+        throw error;
+      }
+      
     })
     .catch((err) => {
       if (!err.statusCode) {
